a80767ab01291fbc702aa95cae9abeb2a4378153
chamothAshen <153191511+chamothAshen@users.noreply.github.com>
2026-01-17 12:15:40 +0530

    add  voice description

--- sigiriya_tour_guide/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sigiriya_tour_guide/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import flutter_inappwebview_macos
+import flutter_tts
 import geolocator_apple
 import package_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
 }
